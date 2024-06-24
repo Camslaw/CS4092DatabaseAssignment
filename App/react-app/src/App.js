0396d5ed97668c0902d4c27435a1925324e8484a
moniesco22 <122181156+moniesco22@users.noreply.github.com>
2024-06-24 19:28:43 -0400

    carousell working

--- App/react-app/src/App.js
+++ App/react-app/src/App.js
@@ -3,6 +3,7 @@ import Navbar from './Navbar';
 import React from 'react';
 // import logo from './logo.svg';   example to import an image
 import './App.css';
+import Carousel from "./carousel"
 
 
 function App() {
@@ -13,10 +14,21 @@ function App() {
           <h1>Add New Customer</h1>
           <CustomerForm />
         </div>
+
+        <div style={{ maxWidth: 1200, marginLeft: 'auto', maarginRight: 'auto', magrinTop: 64}}>
+          <Carousel>
+            <img src={require("./images/products/headphones/beats.webp")} alt="placeholder"/>
+            <img src={require("./images/products/headphones/logi_pink.webp")} alt="placeholder"/>
+            <img src={require("./images/products/headphones/sony_blue.webp")} alt="placeholder"/>
+          </Carousel>
+        </div>
+
+        
       </div>
 
   );
 }
+
 
 
 
---
+++ App/react-app/src/carousel.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import './carousel.css' // added later
+import {useState} from 'react'
+import {useEffect} from 'react'
+
+
+const Carousel = (props) => {
+    const {children} = props
+    const [currentIndex, setCurrentIndex] = useState(0)
+    const [length, setLength] = useState(children.length)
+
+    // Set the length to match current children from props
+    useEffect(() => {
+         setLength(children.length)
+    }, [children])
+
+    const next = () => {
+        if (currentIndex < (length - 1)) {
+            setCurrentIndex(prevState => prevState + 1)
+        }
+    }
+    
+    const prev = () => {
+        if (currentIndex > 0) {
+            setCurrentIndex(prevState => prevState - 1)
+        }
+    }
+    
+    return(
+        <div className="csl-container">
+            <div className="csl-wrapper">
+               { currentIndex > 0 &&
+                    <button onClick={prev} className="left-arrow">
+                        &lt;
+                    </button> 
+                }
+                <div className="csl-content_wrapper">
+                    <div className="csl-content"
+                        style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+                        >
+                            {children}
+                    </div>
+                </div>
+                { currentIndex < (length-1) &&
+                    <button onClick={next} className="right-arrow">
+                        &gt;
+                    </button>
+                }
+            </div>
+        </div>
+    )
+}
+
+export default Carousel
